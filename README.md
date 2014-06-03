dfa2bfdc43a288ec4ef8948fa82f922a1e341c42
essboyer <seanboyer.sj@gmail.com>
2014-06-03 10:45:41 -0300

    Updated README.md Made function anon and immediately executing.

--- README.md
+++ README.md
@@ -10,7 +10,22 @@ It uses &amp; requires [FontAwesome!](http://fortawesome.github.io/Font-Awesome/
 <div id="stars"></div>
 ...
 
-$("#stars").starRating(4.5);
+$("#stars").starRating(3.5);
+
+```
+
+# Result
+The resulting markup produced is minimal and completely controllable with CSS.
+
+```HTML
+
+<div class="starRatingBox">
+	<i class="fa fa-star"></i>
+	<i class="fa fa-star"></i>
+	<i class="fa fa-star"></i>
+	<i class="fa fa-star-half"></i>
+	<i class="fa fa-star-o"></i>
+</div>
 
 ```
 
--- jquery.starRating.js
+++ jquery.starRating.js
@@ -7,35 +7,37 @@
  * @param number double or string (e.g. 1.5 or "2" or "4.5")
  * @returns {jQuery}
  */
-jQuery.fn.starRating = function(number) {
-    var addHalf = false;
-    var count = 0;
-    var $starbox = $('<div class="starRatingBox"></div>');
-    number = String(number);
-    
-    if (number.indexOf(".") === 1) {
-        addHalf = true;
-        count = number.split(".")[0];
-    }
-    else {
-        count = number;
-    }
-    
-    for (var i = 1; i <= 5; i++) {
-        if (i <= count) {
-            $starbox.append('<i class="fa fa-star"></i>');
-        }
-        
-        if (i > count) {
-            if (addHalf) {
-                $starbox.append('<i class="fa fa-star-half"></i>');
-                addHalf = false;
-                continue;
-            }
-            
-            $starbox.append('<i class="fa fa-star-o"></i>');
-        }
-    }
+(function($) {
+	$.fn.starRating = function(number) {
+		var addHalf = false;
+		var count = 0;
+		var $starbox = $('<div class="starRatingBox"></div>');
+		number = String(number);
+		
+		if (number.indexOf(".") === 1) {
+			addHalf = true;
+			count = number.split(".")[0];
+		}
+		else {
+			count = number;
+		}
+		
+		for (var i = 1; i <= 5; i++) {
+			if (i <= count) {
+				$starbox.append('<i class="fa fa-star"></i>');
+			}
+			
+			if (i > count) {
+				if (addHalf) {
+					$starbox.append('<i class="fa fa-star-half"></i>');
+					addHalf = false;
+					continue;
+				}
+				
+				$starbox.append('<i class="fa fa-star-o"></i>');
+			}
+		}
 
-    return this.append($starbox);
-}
+		return this.append($starbox);
+	}
+}(jQuery));
